b9dc85195987cdf1dd0ec5738958c054e3517009
unknown <mywork00500@gmail.com>
2019-09-14 12:45:57 +0530

    Updates

---
+++ precache-manifest.3324ac3004ff39aed1263b2320820846.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8973e91121f217cde0a0b1c4c9d288ab",
+    "url": "/my-work-005/website.git/index.html"
+  },
+  {
+    "revision": "3a22423b4fb8145762a3",
+    "url": "/my-work-005/website.git/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "c290e6b4140c0eac94c2",
+    "url": "/my-work-005/website.git/static/js/2.de60ffef.chunk.js"
+  },
+  {
+    "revision": "3a22423b4fb8145762a3",
+    "url": "/my-work-005/website.git/static/js/main.fa09945d.chunk.js"
+  },
+  {
+    "revision": "cd60e176f0f1db0a81e6",
+    "url": "/my-work-005/website.git/static/js/runtime~main.935e2fdf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-work-005/website.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
